0800f0bec5c90096167778d0c58b403c847d8cc8
khaled romdhane <khaled@medgo.fr>
2020-10-08 10:01:42 +0200

    adding an api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import * as express from "express";
+import * as cors from "cors";
 
 const app = express();
+app.use(cors());
+app.get("/api", (req, res) => {
+  res.json({
+    msg: "Edit src/App.js and save to reload",
+  });
+});
 
-app.listen(3000);
+app.listen(4000);
